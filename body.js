1ae42460de7aad247803199b94cfb41080440f80
alex <jandroz@gmail.com>
2018-04-24 01:16:19 +0200

    Add Body class

---
+++ body.js
@@ -0,0 +1,20 @@
+
+
+let Body = function() {
+    this.gravity = false;
+    this.vx = 0;
+    this.vy = 0;
+    this.radius = 0;
+    this.x = 0;
+    this.y = 0;
+    this.mass = 0;
+    this.type = 0
+    this.color = 0;
+};
+
+Body.BODY_TYPES = {
+    STAR: 1,
+    PLANET: 2
+};
+
+module.exports = Body;
