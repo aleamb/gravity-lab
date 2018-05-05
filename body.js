2f7929d9614b85fc2ac81c1cbd2ee3a8548b9357
aleamb <jandroz@gmail.com>
2018-05-05 15:44:06 +0200

    Remove CRLF

--- body.js
+++ body.js
@@ -1,20 +1,20 @@
 
 
-let Body = function() {
-    this.gravity = true;
-    this.vx = 0;
-    this.vy = 0;
-    this.radius = 0;
-    this.x = 0;
-    this.y = 0;
-    this.mass = 0;
-    this.type = 0
-    this.color = 0;
+const Body = function () {
+  this.gravity = true;
+  this.vx = 0;
+  this.vy = 0;
+  this.radius = 0;
+  this.x = 0;
+  this.y = 0;
+  this.mass = 0;
+  this.type = 0;
+  this.color = 0;
 };
 
 Body.BODY_TYPES = {
-    STAR: 1,
-    PLANET: 2
+  STAR: 1,
+  PLANET: 2,
 };
 
 module.exports = Body;
